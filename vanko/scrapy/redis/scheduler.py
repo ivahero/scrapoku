d30bd761fac26e87a6b13ce070af98e41f8299db
ivandeex <ivandeex@gmail.com>
2016-01-27 06:20:21 +0300

    scrapy redis: repair scrapy shell

--- vanko/scrapy/redis/scheduler.py
+++ vanko/scrapy/redis/scheduler.py
@@ -25,7 +25,7 @@ class Scheduler(object):
     """Redis-based scheduler"""
 
     def __init__(self, server, persist, queue_key, queue_cls, dupefilter_key,
-                 idle_before_close):
+                 idle_before_close, unser_df, unser_queue_cls):
         """Initialize scheduler.
 
         Parameters
@@ -44,6 +44,8 @@ class Scheduler(object):
         self.dupefilter_key = dupefilter_key
         self.idle_before_close = idle_before_close
         self.stats = None
+        self.unser_df = unser_df
+        self.unser_queue_cls = unser_queue_cls
 
     def __len__(self):
         return len(self.queue)
@@ -58,8 +60,11 @@ class Scheduler(object):
         idle_before_close = settings.get('SCHEDULER_IDLE_BEFORE_CLOSE',
                                          IDLE_BEFORE_CLOSE)
         server = connection.from_settings(settings)
+        unser_dupefilter_cls = load_object(settings['DUPEFILTER_CLASS'])
+        unser_df = unser_dupefilter_cls.from_settings(settings)
+        unser_queue_cls = load_object(settings.get('SCHEDULER_MEMORY_QUEUE'))
         return cls(server, persist, queue_key, queue_cls, dupefilter_key,
-                   idle_before_close)
+                   idle_before_close, unser_df, unser_queue_cls)
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -73,6 +78,7 @@ class Scheduler(object):
         self.queue = self.queue_cls(self.server, spider, self.queue_key)
         self.df = RFPDupeFilter(self.server,
                                 self.dupefilter_key % {'spider': spider.name})
+        self.unser_queue = self.unser_queue_cls()
         if self.idle_before_close < 0:
             self.idle_before_close = 0
         # notice if there are requests already in the queue to resume the crawl
@@ -86,6 +92,15 @@ class Scheduler(object):
             self.queue.clear()
 
     def enqueue_request(self, request):
+        if not request_is_serializable(request):
+            if not request.dont_filter and self.unser_df.request_seen(request):
+                return
+            if self.stats:
+                self.stats.inc_value('scheduler/enqueued/unser',
+                                     spider=self.spider)
+            self.unser_queue.push(request)
+            return
+
         if not request.dont_filter and self.df.request_seen(request):
             return
         if self.stats:
@@ -94,6 +109,12 @@ class Scheduler(object):
         self.queue.push(request)
 
     def next_request(self):
+        request = self.unser_queue.pop()
+        if request is not None:
+            if self.stats:
+                self.stats.inc_value('scheduler/dequeued/unser',
+                                     spider=self.spider)
+            return request
         block_pop_timeout = self.idle_before_close
         request = self.queue.pop(block_pop_timeout)
         if request and self.stats:
@@ -103,3 +124,21 @@ class Scheduler(object):
 
     def has_pending_requests(self):
         return len(self) > 0
+
+
+def request_is_serializable(request):
+    for cb in request.callback, request.errback:
+        if cb is None or isinstance(cb, basestring):
+            continue
+        if isinstance(cb, types.MethodType):
+            obj = six.get_method_self(cb)
+            if not isinstance(obj, Spider):
+                return False
+            func = six.get_method_function(cb)
+            attr = getattr(obj, func.__name__, None)
+            if callable(attr):
+                continue
+        if isinstance(cb, types.FunctionType) and cb.__name__.isalnum():
+            continue
+        return False
+    return True
